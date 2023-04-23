13d21d6af3efda0d61f32a13d6ca87c92e1b32da
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2023-04-23 14:09:16 +0000

    Handle images

--- public/styling/scripts/element-list.js
+++ public/styling/scripts/element-list.js
@@ -7,4 +7,7 @@ const pageTitle = document.getElementById("page-title"); // page title
 const picButton = document.getElementById("pic-button"); // Pic button
 
 // Image Form Elements
+const imageCanvas = document.getElementById("image-canvas"); // canvas handler
+const ctx = imageCanvas.getContext('2d');
+
 const imageFile = document.getElementById("imageFile"); // img file
